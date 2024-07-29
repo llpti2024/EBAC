e7205e19baff9c39912f534b851650bd9a41142b
Lucas Levy Peruchi <llpti2024@gmail.com>
2024-07-29 10:13:58 -0300

    Tarefa De Media Notas

---
+++ TarefaCalculoMedia/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+    Notas notas = new Notas();
+        System.out.println(notas.getMedia());
+    }
+}
---
+++ TarefaCalculoMedia/src/Notas.java
@@ -0,0 +1,16 @@
+public class Notas {
+    private double notaUm = 4;
+    private double notaDois = 7.5;
+    private double notaTres = 5;
+    private double notaQuatro = 9.3;
+    private double totalNotas = 4;
+    private double media = (this.notaUm+this.notaDois+this.notaTres+this.notaQuatro)/totalNotas;
+
+
+    public double getMedia() {
+        return media;
+    }
+}
+
+
+
