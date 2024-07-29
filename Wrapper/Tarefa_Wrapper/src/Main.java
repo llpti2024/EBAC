44df80b45c413fc0b5d22dba3cf1fca0197d5064
Lucas Levy Peruchi <llpti2024@gmail.com>
2024-07-29 11:24:25 -0300

    Atualização Exercício Wrapper

---
+++ Wrapper/Tarefa_Wrapper/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Wrapper w = new Wrapper();
+        System.out.println("O (Valor 1) é " + w.valor + ", o (Valor 2) é " + w.valor2 + ", o (Valor 3) é " + w.valor3 + ", o (Valor 4) é " + w.valor4 + ", o (Valor 5) é "+ w.valor5);
+    }
+}
---
+++ Wrapper/Tarefa_Wrapper/src/Wrapper.java
@@ -0,0 +1,7 @@
+public class Wrapper {
+    public int valor = new Integer(1);
+    public int valor2 = new Integer(2);
+    public long valor3 = 555;
+    public int valor4 = (int) valor3;
+    public Integer valor5 = 5;
+}
